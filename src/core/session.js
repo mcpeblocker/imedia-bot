8f421489f43c337f9d895c6d2079fd4905e3e235
mcpeblocker <mcpeblocker@gmail.com>
2022-01-15 22:43:44 +0500

    switch session with .env var

--- src/core/session.js
+++ src/core/session.js
@@ -1,19 +1,22 @@
 // // Memory session
-// const { session: memorySession } = require('telegraf');
-
-// const session = memorySession();
-
+const { session: memorySession } = require('telegraf');
 
 
 // Redis session
 const RedisSession = require('telegraf-session-redis');
 const config = require('../utils/config');
 
-const session = new RedisSession({
-    store: {
-        host: config.REDIS_HOST || '127.0.0.1',
-        port: config.REDIS_PORT || 6379
-    }
-});
+let session;
+
+if (config.SESSION_TYPE === 'redis') {
+    session = new RedisSession({
+        store: {
+            host: config.REDIS_HOST || '127.0.0.1',
+            port: config.REDIS_PORT || 6379
+        }
+    });;
+} else {
+    session = memorySession;
+}
 
 module.exports = session;
--- src/utils/config.js
+++ src/utils/config.js
@@ -2,6 +2,7 @@ const config = {
     BOT_TOKEN: process.env.BOT_TOKEN,
     NODE_ENV: process.env.NODE_ENV,
     ADMINS: process.env.ADMINS.split(","),
+    SESSION_TYPE: process.env.SESSION_TYPE,
     REDIS_HOST: process.env.REDIS_HOST,
     REDIS_PORT: process.env.REDIS_PORT,
     DB_URI: process.env.DB_URI
